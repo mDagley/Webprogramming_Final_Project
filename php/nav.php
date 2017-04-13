bf6d096718c1688350bfe186052c5725dda6eeb0
Melissa Dagley <Mxd153730@utdallas.edu>
2017-04-13 15:06:32 -0500

    Add files via upload

---
+++ php/nav.php
@@ -0,0 +1,17 @@
+
+
+
+<header>
+    <a href="index.php"><img src="../BookStore/img/banner.jpg" class="bannerimage"></a>
+        </header>
+        <nav>
+            <ul> 
+                <li><a href="index.php">Home</a></li>
+                <li class="right"><a href="cart.php">Cart (2)</a></li>
+                <li class="right"><a href="account.php">Account</a></li>
+                <li class="right"><a href="login.php">Login</a></li>
+                <li class="right"><a href="new.php">Register</a></li>
+            </ul>
+        </nav>
+ 
+ 
