b1f4393bc134046d699bf51fe4e601672b4c2453
Melissa Dagley <Mxd153730@utdallas.edu>
2017-04-23 14:11:27 -0500

    Admin: Add Book
    Adding a book is now working except for uploading the picture file. I
    still need to enable add author and add subgenre. And then I need to
    work on edit book. But those should all be easier.

--- php/book.php
+++ php/book.php
@@ -1,29 +1,32 @@
 <?php
 include "connect.php" ;
-$testing = true;
+$testing = false;
 
 function test_input($data)
 {   $data = trim($data); 
     $data = stripslashes($data); 
-    $data = htmlspecialchars($data);
+    $data = htmlspecialchars($data, ENT_QUOTES);
     return $data;
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {   
-    $isbn     = test_input($_POST['isbn1']);
-    $binding  = test_input($_POST['binding1']);
-    $title        = test_input($_POST['title1']);
+    $isbn           = test_input($_POST['isbn1']);
+    $binding        = test_input($_POST['binding1']);
+    $title          = test_input($_POST['title1']);
     $publish        = test_input($_POST['publish1']);
-    $publisher        = test_input($_POST['publisher1']);
-    $pages   = test_input($_POST['pages1']); 
-    $genre      = test_input($_POST['genre1']);
-    $price     = test_input($_POST['price1']);
-    $qty       = test_input($_POST['qty1']);
-    $author   = test_input($_POST['author1']);
-    $description     = test_input($_POST['description1']);
-    $subgenre = test_input($_POST['subgenre1']);
-    $img = test_input($_POST['img1']);
+    $publisher      = test_input($_POST['publisher1']);
+    $pages          = test_input($_POST['pages1']); 
+    $genre          = test_input($_POST['genre1']);
+    $price          = test_input($_POST['price1']);
+    $qty            = test_input($_POST['qty1']);
+    $author         = test_input($_POST['author1']);
+    $authors        = explode(", ",$author);
+    $description    = test_input($_POST['description1']);
+    $subgenre       = test_input($_POST['subgenre1']);
+    $subgenres      = explode(", ",$subgenre);
+    $img            = test_input($_POST['img1']);
+    $imgName        = explode(" ",$img);
    
 }
 
@@ -46,12 +49,36 @@ else if($testing == true){
     echo "Author: ".$author."\n";
     echo "Description: ".$description."\n";
     echo "Subgenre: ".$subgenre."\n";
-    echo "Image File: ".$img."\n";
+    echo "Image File: ".$imgName."\n";
 }
 
 else  
 {   
-    //Insert values into tables
+    //Need to figure out multiple authors and subgenres
+    $booksql = "INSERT INTO books (ISBN13, Binding, Title, PublishDate, Publisher, Pages, GenreId, Price, Qty, Description, CoverImage)
+                VALUES('".$isbn."', '".$binding."', '".$title."', '".$publish."', '".$publisher."', '".$pages."', '".$genre."', '".$price."', '".$qty."', '".$description."', '".$imgName[0]."');";
+    $con->query($booksql);
+    echo $booksql;
+    
+    $id = "SELECT Id FROM books WHERE ISBN13='".$isbn."'";
+    $result=mysqli_query($con, $id);
+    $book_id = mysqli_fetch_row($result);
+    echo $id;
+    echo $book_id;
+    
+    foreach($authors as $a){
+    $authorsql ="INSERT INTO authorbook (BookId, AuthorId)
+                VALUES(".$book_id[0].", ".$a.");";
+    $con->query($authorsql);
+        echo $authorsql;
+    }
+    
+    foreach($subgenres as $s){
+    $subgenresql= "INSERT INTO subgenrebook (BookId, SunGenreId)
+                VALUES(".$book_id[0].", ".$s.");";
+    $con->query($subgenresql);
+        echo $subgenresql;
+    }
     
 }
 
